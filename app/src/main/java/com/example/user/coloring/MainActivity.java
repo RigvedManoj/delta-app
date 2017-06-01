8a3fe2490abb6e926c5a82a4a4ef19b741726d6d
Voltura5 <rigvedmanoj1998@gmail.com>
2017-06-01 18:03:06 +0530

    initial

--- app/src/main/java/com/example/user/coloring/MainActivity.java
+++ app/src/main/java/com/example/user/coloring/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.user.coloring;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -11,11 +13,39 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private RelativeLayout rl;
-    int i=0,j=0,k=0;
+    int i,j,k;
+    public static final String a1 = "MyPrefsFile";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences sharedPrefs1 = getApplicationContext().getSharedPreferences(a1, Context.MODE_PRIVATE);
+        i= sharedPrefs1.getInt("counter1", 0);
+        SharedPreferences sharedPrefs2 = getApplicationContext().getSharedPreferences(a1, Context.MODE_PRIVATE);
+         j= sharedPrefs2.getInt("counter2", 0);
+        SharedPreferences sharedPrefs3 = getApplicationContext().getSharedPreferences(a1, Context.MODE_PRIVATE);
+         k= sharedPrefs3.getInt("counter3", 0);
+        TextView m1 = (TextView) findViewById(R.id.textView);
+        m1.setText(""+i);
+         TextView m2 = (TextView) findViewById(R.id.textView3);
+        m2.setText(""+j);
+         TextView m3 = (TextView) findViewById(R.id.textView2);
+        m3.setText(""+k);
+        rl = (RelativeLayout) this.findViewById(R.id.rellay);
+        rl.setBackgroundColor(Color.argb(255, i, j, k));
+    }
+    public void Reset(View view) {
+        i=0;
+        j=0;
+        k=0;
+        TextView m1 = (TextView) findViewById(R.id.textView);
+        m1.setText(""+i);
+        TextView m2 = (TextView) findViewById(R.id.textView3);
+        m2.setText(""+j);
+        TextView m3 = (TextView) findViewById(R.id.textView2);
+        m3.setText(""+k);
+        rl = (RelativeLayout) this.findViewById(R.id.rellay);
+        rl.setBackgroundColor(Color.argb(255, i, j, k));
     }
     public void red(View view) {
         if (i < 255)
@@ -47,5 +77,14 @@ public class MainActivity extends AppCompatActivity {
         final TextView m1 = (TextView) findViewById(R.id.textView3);
         m1.setText(""+j);
     }
-
+    @Override
+    protected void onStop(){
+        super.onStop();
+        SharedPreferences settings = getSharedPreferences(a1, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt("counter1", i);
+        editor.putInt("counter2", j);
+        editor.putInt("counter3", k);
+        editor.commit();
+    }
 }
